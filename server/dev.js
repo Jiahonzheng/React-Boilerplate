69aa910ae31db995be152c8967a26bf8b094f620
Mr. Tseng <jiahonzheng@gmail.com>
2018-07-07 23:47:04 +0800

    拆分 Dev 和 Prod 服务器

---
+++ server/dev.js
@@ -0,0 +1,32 @@
+const bs = require("browser-sync").create();
+const express = require("express");
+const webpack = require("webpack");
+const config = require("../build/webpack.client.dev");
+
+const PORT = 8887;
+const app = express();
+const worker = webpack(config);
+
+app.use(
+  require("webpack-dev-middleware")(worker, {
+    publicPath: config.output.publicPath,
+    hot: true,
+    historyApiFallback: true,
+    inline: true,
+    progress: true,
+    stats: {
+      colors: true
+    }
+  })
+);
+app.use(require("webpack-hot-middleware")(worker));
+
+bs.init({
+  open: true,
+  ui: false,
+  notify: false,
+  proxy: "localhost:8887",
+  port: 8080
+});
+
+app.listen(PORT);
